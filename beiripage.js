a6183f22e178710aaf975fb46649a4751e670aa9
Toni Penya-Alba <toni.penya.alba@gmail.com>
2015-04-15 10:29:39 +0200

    cover position fixed

--- beiripage.js
+++ beiripage.js
@@ -1,5 +1,4 @@
 var app = angular.module('BeiriPage', ['youtube-embed',
-                                        // 'ngAnimate',
                                         'duScroll']);
 
 app.value('duScrollDuration', 500);
